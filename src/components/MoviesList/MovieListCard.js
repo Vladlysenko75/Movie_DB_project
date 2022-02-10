4647c0f84ad1ff5129a1844397b5954b24cc452b
Vladlysenko75 <vladyslavlysenko75@gmail.com>
2022-02-10 19:00:36 +0200

    files

--- src/components/MoviesList/MovieListCard.js
+++ src/components/MoviesList/MovieListCard.js
@@ -4,26 +4,29 @@ import {useSelector} from "react-redux";
 import {urls} from "../../constants";
 import "./MovieListCard.css";
 import rating from '../../img/rating.svg';
+import {Link} from "react-router-dom";
 
 export const MovieListCard = ({movie}) => {
-    const {title, vote_average, poster_path, id, genre_ids} = movie;
+    const {vote_average, poster_path, id, genre_ids} = movie;
     const {genres} = useSelector(state => state['genresReducer']);
 
     const filteredGenre = genres.filter(genre => genre.id === genre_ids[0])[0];
 
     return (
         <div  className='movie'>
-            <div style={{backgroundImage: `url(${urls.img185.concat(poster_path)})`}} className="movieInfo">
-                <div className="info">
-                    <div className="genreBadge">
-                        <p>{filteredGenre && filteredGenre.name}</p>
-                    </div>
-                    <div className="popularRating">
-                        <p>{vote_average}</p>
-                        <img src={rating}  alt="rating"/>
+            <Link to={`/movie/${id}`} style={{ textDecoration: 'none' }}>
+                <div style={{backgroundImage: `url(${urls.img185.concat(poster_path)})`}} className="movieInfo">
+                    <div className="info">
+                        <div className="genreBadge">
+                            <p>{filteredGenre && filteredGenre.name}</p>
+                        </div>
+                        <div className="popularRating">
+                            <p>{vote_average}</p>
+                            <img src={rating}  alt="rating"/>
+                        </div>
                     </div>
                 </div>
-            </div>
+            </Link>
         </div>
     );
 };
--- src/components/TopRated/TopRatedMovie.js
+++ src/components/TopRated/TopRatedMovie.js
@@ -3,25 +3,28 @@ import {useSelector} from "react-redux";
 
 import {urls} from "../../constants";
 import rating from "../../img/rating.svg";
+import {Link} from "react-router-dom";
 
 export const TopRatedMovie = ({movie}) => {
-    const {title, vote_average, poster_path, id, genre_ids} = movie;
+    const {vote_average, poster_path, id, genre_ids} = movie;
     const {genres} = useSelector(state => state['genresReducer']);
     const filteredGenre = genres.filter(genre => genre.id === genre_ids[0])[0];
 
     return (
         <div>
-            <div style={{backgroundImage: `url(${urls.img342.concat(poster_path)})`}} className="movieInfo">
-                <div className="info">
-                    <div className="genreBadge">
-                        <p>{filteredGenre && filteredGenre.name}</p>
-                    </div>
-                    <div className="popularRating">
-                        <p>{vote_average}</p>
-                        <img src={rating}  alt="rating"/>
+            <Link to={`/movie/${id}`} style={{ textDecoration: 'none' }}>
+                <div style={{backgroundImage: `url(${urls.img342.concat(poster_path)})`}} className="movieInfo">
+                    <div className="info">
+                        <div className="genreBadge">
+                            <p>{filteredGenre && filteredGenre.name}</p>
+                        </div>
+                        <div className="popularRating">
+                            <p>{vote_average}</p>
+                            <img src={rating}  alt="rating"/>
+                        </div>
                     </div>
                 </div>
-            </div>
+            </Link>
         </div>
     );
 };
